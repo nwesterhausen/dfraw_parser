26f0ecd5e14e0db146f0cc343f070c381792f20f
Nick Westerhausen <nwesterhausen@gmail.com>
2024-11-29 08:56:20 -0500

    feat: add tables to support requirement/conflict info

---
+++ migrations/2024-11-29-135011_info_file_ids/down.sql
@@ -0,0 +1,8 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE info_file_requires_ids;
+
+DROP TABLE info_file_conflicts_ids;
+
+DROP TABLE info_file_requires_before;
+
+DROP TABLE info_file_requires_after;
