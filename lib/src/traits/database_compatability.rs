886ab2e26e13e51aeeeadd440f179b7e62b6fb8d
Nick Westerhausen <nwesterhausen@gmail.com>
2025-03-18 08:32:32 -0400

    feat: create trait for database_compatability.rs

---
+++ lib/src/traits/database_compatability.rs
@@ -0,0 +1,33 @@
+//! These traits are used to provide a common interface for various parsed objects. They are used
+//! to simplify the process of storing and searching for these objects in the database.
+//!
+//! It exists to simplify the process of inserting a new record into the database. For example,
+//! the `[Creature]` struct implements this trait, so you can insert a new creature into the
+//! database without having to worry about the details of how to do so.
+
+/// This trait is used to insert a new record into the database.
+///
+/// It provides a method to get the SQL required to insert the record into the database.
+#[typetag::serde(tag = "type")]
+pub trait Insertable {
+    /// Returns the SQL required to insert the record into the database.
+    fn to_insert_sql(&self) -> String;
+}
+
+/// This trait is used to get an object out of the database.
+///
+/// It provides methods to query the database and return the object.
+///
+/// More methods to be added if needed.
+#[typetag::serde(tag = "type")]
+pub trait Queryable {
+    /// Will query the database and return the object, by its identifier.
+    fn get_by_identifier(identifier: &str) -> Option<Self>
+    where
+        Self: Sized;
+
+    /// Will query the database and return the object, by its id.
+    fn get_by_id(id: i32) -> Option<Self>
+    where
+        Self: Sized;
+}
--- lib/src/traits/mod.rs
+++ lib/src/traits/mod.rs
@@ -1,6 +1,7 @@
 //! Shared traits amongst various parsed objects.
 
 pub mod creature_variation_requirements;
+pub mod database_compatability;
 pub mod raw_object;
 pub mod raw_object_token;
 pub mod raw_object_token_to_any;
@@ -8,6 +9,7 @@ pub mod searchable;
 pub mod tag_operations;
 
 pub use creature_variation_requirements::CreatureVariationRequirements;
+pub use database_compatability::{Insertable, Queryable};
 pub use raw_object::RawObject;
 pub use raw_object_token::RawObjectToken;
 pub use raw_object_token_to_any::RawObjectTokenToAny;
