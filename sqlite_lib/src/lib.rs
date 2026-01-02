460dffa4cd1847fc65e0007134fd4bb4b29ca3b5
Nick Westerhausen <nwesterhausen@gmail.com>
2026-01-02 10:28:40 -0500

    update sqlite lib header

--- sqlite_lib/src/lib.rs
+++ sqlite_lib/src/lib.rs
@@ -1,4 +1,9 @@
 //! This library provides a way to interact with a database, specifically for storing the DF Raw data.
+//!
+//! # Note
+//!
+//! This is an experimental library, it is not yet ready for production use. Currently it only will create a database and may function to insert
+//! or read data but has not been thoroughly tested.
 
 pub mod client;
 pub(crate) mod db_init;
