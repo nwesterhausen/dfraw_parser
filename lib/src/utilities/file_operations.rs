9c2af9b27351d458d58f9b4d2ebce23c8993c8cc
Nick Westerhausen <nwesterhausen@gmail.com>
2026-01-20 15:35:15 -0500

    remove un-needed object_id generator

--- lib/src/utilities/file_operations.rs
+++ lib/src/utilities/file_operations.rs
@@ -14,7 +14,6 @@ use std::{
 
 use itertools::Itertools;
 use lazy_regex::regex;
-use slug::slugify;
 use tracing::{debug, error, info, trace, warn};
 use uuid::Uuid;
 use walkdir::WalkDir;
@@ -22,7 +21,7 @@ use walkdir::WalkDir;
 use crate::{
     Creature, CreatureVariation, Entity, Graphic, Inorganic, MaterialTemplate, ParserError, Plant,
     SelectCreature, TilePage,
-    metadata::{ParserOptions, RawMetadata, RawModuleLocation},
+    metadata::{ParserOptions, RawModuleLocation},
     regex::VARIATION_ARGUMENT_RE,
     tags::ObjectType,
     traits::{CreatureVariationRequirements, IsEmpty, RawObject},
@@ -528,34 +527,6 @@ pub fn log_summary<S: BuildHasher>(summary: &HashMap<ObjectType, usize, S>) {
         info!("\t{count}\t{object_type}");
     }
     info!("Total: {total}");
-}
-
-/// The function `build_object_id_from_pieces` takes in metadata, an identifier, and a raw type, and
-/// returns a formatted string. This is a convenience function for building object IDs so they all
-/// follow the same format.
-///
-/// Arguments:
-///
-/// * `metadata`: A reference to a `RawMetadata` object.
-/// * `identifier`: A string representing the identifier of the object.
-/// * `raw_type`: The `raw_type` parameter is of type `ObjectType`.
-///
-/// Returns:
-///
-/// a string.
-#[must_use]
-pub fn build_object_id_from_pieces(
-    metadata: &RawMetadata,
-    identifier: &str,
-    raw_type: &ObjectType,
-) -> String {
-    format!(
-        "{raw_parent_id}-{raw_type}-{raw_id}-{module_name}{module_version}",
-        raw_id = slugify(identifier),
-        raw_parent_id = slugify(metadata.get_raw_identifier()),
-        module_version = metadata.get_module_version(),
-        module_name = slugify(metadata.get_module_name()),
-    )
 }
 
 /// It takes a slice of strings, parses the first two strings as unsigned 16-bit integers, and returns a
