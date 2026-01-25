51dbe139aa3d9db4b321b30e93083b6a6690d57f
Nick Westerhausen <nwesterhausen@gmail.com>
2026-01-24 19:19:20 -0500

    switch to std library equivalent

--- lib/src/constants.rs
+++ lib/src/constants.rs
@@ -1,14 +1,13 @@
 //! Constants used throughout the library
 
+use std::sync::LazyLock;
+
 use uuid::Uuid;
 
 /// The encoding used by dwarf fortress text files
-pub static DF_ENCODING: once_cell::sync::Lazy<&encoding_rs::Encoding> =
-    once_cell::sync::Lazy::new(|| {
-        encoding_rs::Encoding::for_label(b"latin1").unwrap_or_else(|| {
-            panic!("Failed to get encoding: latin1");
-        })
-    });
+pub static DF_ENCODING: LazyLock<&'static encoding_rs::Encoding> = LazyLock::new(|| {
+    encoding_rs::Encoding::for_label(b"latin1").expect("Failed to get encoding: latin1")
+});
 
 /// The Steam AppID for Dwarf Fortress
 pub const DF_STEAM_APPID: u32 = 975370;
